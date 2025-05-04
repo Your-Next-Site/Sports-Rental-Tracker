90070bc7fd6bacc9ccdb3dce55ee0596ca42ce8f
Patrick MacDonald <patrick@patmac.ca>
2025-05-04 17:42:59 -0600

    links added

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center h-screen p-2 pb-20 font-[family-name:var(--font-geist-sans)]">
       <MainContainer>
-        <div>Please check out
+        <div className="mt-auto mx-auto">Please check out
           <Link className="underline" href="/terms-of-service" > Terms of Service </Link>
           and our
           <Link className="underline" href='/privacy-policy'> Privacy Policy.</Link></div>
