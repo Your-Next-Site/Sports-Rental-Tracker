f6bf2290172ee38f8a30458e7a17f02b45745543
Patrick MacDonald <patrick@patmac.ca>
2025-05-08 17:54:04 -0600

    added text for when one is on the water

--- components/ui/on-the-water-component/on-the-water.tsx
+++ components/ui/on-the-water-component/on-the-water.tsx
@@ -17,21 +17,24 @@ export default function OnTheWater() {
                     {isError &&
                         <p className="text-red-500">Error marking raft arrived</p>
                     }
-                    {data?.map((trip, index) => (
-                        <div key={index} className="bg-white p-4 rounded shadow-2xl md:w-1/2 lg:w-1/3 xl:w-1/4">
-                            <h2 className="text-lg">{trip.guest_name}</h2>
-                            <p>Departure Time:{new Date(trip.departure_time).toLocaleString('en-CA', { dateStyle: 'short', timeStyle: 'short' })} </p>
-                            <p>Raft Size: {trip.raft_type_name}</p>
-                            <p>Unit Number: {trip.unit_number}</p>
-                            <button
-                                onClick={trip_id => mutate(trip.id)}
-                                disabled={isPending}
-                                className="bg-buttoncolormain hover:bg-buttoncolorsecend hover:text-white p-4 mt-auto md:w-full text-center mx-auto shadow-lg">
-                                Mark Arrived
-                            </button>
-
-                        </div>
-                    ))}
+                    {data && data.length > 0 ? (
+                        data.map((trip, index) => (
+                            <div key={index} className="bg-white p-4 rounded shadow-2xl md:w-1/2 lg:w-1/3 xl:w-1/4">
+                                <h2 className="text-lg">{trip.guest_name}</h2>
+                                <p>Departure Time:{new Date(trip.departure_time).toLocaleString('en-CA', { dateStyle: 'short', timeStyle: 'short' })} </p>
+                                <p>Raft Size: {trip.raft_type_name}</p>
+                                <p>Unit Number: {trip.unit_number}</p>
+                                <button
+                                    onClick={trip_id => mutate(trip.id)}
+                                    disabled={isPending}
+                                    className="bg-buttoncolormain hover:bg-buttoncolorsecend hover:text-white p-4 mt-auto md:w-full text-center mx-auto shadow-lg">
+                                    Mark Arrived
+                                </button>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="bg-white p-4 rounded shadow-2xl md:w-1/2 lg:w-1/3 xl:w-1/4">No one is on the water</p>
+                    )}
                 </div>
             </div>
         </MainContainer >
