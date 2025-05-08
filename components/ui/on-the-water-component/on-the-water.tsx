aff87115ed8a2896f012c06dac29ed0e4bf8aab6
Patrick MacDonald <patrick@patmac.ca>
2025-05-07 22:50:02 -0600

    fixed error

--- components/ui/on-the-water-component/on-the-water.tsx
+++ components/ui/on-the-water-component/on-the-water.tsx
@@ -2,10 +2,10 @@
 import MainContainer from "../containers/main-container";
 import { useGetTrips } from "@/hooks/hooks";
 
-export default function OnTheWater() { 
+export default function OnTheWater() {
 
-const { data } = useGetTrips();
-console.log(data)
+    const { data } = useGetTrips();
+    console.log(data)
 
     return (
         <MainContainer>
@@ -14,12 +14,10 @@ console.log(data)
                 <div className="flex flex-wrap justify-center gap-4">
                     {data?.map((trip, index) => (
                         <div key={index} className="bg-white p-4 rounded shadow-2xl md:w-1/2 lg:w-1/3 xl:w-1/4">
-                            <h2 className="text-lg">{trip.guest_name}</h2>
-                            {/* <p>{trip.departure_time}</p> */}
-                            {/* <p>Departure Time: {new Date(trip.departure_time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p> */}
-                            <p>{new Date(trip.departure_time).toLocaleString()}</p>
+                            <h2 className="text-lg">{trip.guest_name}</h2>                           
+                            <p>Departure Time:{new Date(trip.departure_time).toLocaleString('en-CA', { dateStyle: 'short', timeStyle: 'short' })} </p>                            
                             <p>Raft Size: {trip.raft_type_name}</p>
-                            <p>Unit Number: {trip.id}</p>
+                            <p>Unit Number: {trip.id}</p> 
                             <button className="bg-buttoncolormain hover:bg-buttoncolorsecend hover:text-white p-4 mt-auto md:w-full text-center mx-auto shadow-lg">
                                 Mark Arrived
                             </button>
