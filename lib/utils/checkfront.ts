19a7c59b39fa67f09f756893110c0aac816023af
Patrick MacDonald <patrick@patmac.ca>
2025-05-15 20:48:27 -0600

    api replying correctly

--- lib/utils/checkfront.ts
+++ lib/utils/checkfront.ts
@@ -1,9 +1,12 @@
 export async function fetchBookings() {
+    const apiKey = process.env.API_KEY_CHECKFRONT;
+    const apiSecret = process.env.API_SECRET_CHECKFRONT;
+    const auth = Buffer.from(`${apiKey}:${apiSecret}`).toString('base64');
+
     const response = await fetch('https://thepaddlestation-2025test.checkfront.com/api/3.0/booking', {
         method: 'GET',
         headers: {
-            'API-Key': `${process.env.API_KEY_CHECKFRONT}`,
-            'API-Secret': `${process.env.API_SECRET_CHECKFRONT}`
+            'Authorization': `Basic ${auth}`
         }
     });
     return await response.json();
