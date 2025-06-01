5ab78c140f7a14c32e746140804e0160053edb1c
Patrick MacDonald <patrick@patmac.ca>
2025-06-01 17:07:55 -0600

    removed twitter

--- auth.ts
+++ auth.ts
@@ -1,9 +1,4 @@
 import NextAuth from 'next-auth';
-import Google from 'next-auth/providers/google'
-import GitHub from 'next-auth/providers/github';
-import GitLab from 'next-auth/providers/gitlab'
-import Discord from 'next-auth/providers/discord';
-import Twitter from 'next-auth/providers/twitter';
 import NeonAdapter from "@auth/neon-adapter"
 import { Pool } from "@neondatabase/serverless"
 import authConfig from './auth.config';
