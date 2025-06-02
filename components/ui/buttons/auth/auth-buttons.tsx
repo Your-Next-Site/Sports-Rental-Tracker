e609d617cd4d24e8e758957fa86bde546d2b6251
Patrick MacDonald <patrick@patmac.ca>
2025-06-01 19:08:37 -0600

    code clean up

--- components/ui/buttons/auth/auth-buttons.tsx
+++ components/ui/buttons/auth/auth-buttons.tsx
@@ -7,20 +7,18 @@ import { AuthButtonProps } from '@/types/types';
 
 export default function Authbutton({ session }: AuthButtonProps) {
     return (
-        <>
-            <div className='flex flex-wrap gap-4 w-full '>
-                {!session ? (
-                    <div className="flex flex-col md:flex-row justify-center gap-2 p-4 w-full">
-                        <AuthButton icon={faGoogle} label="Sign In With Google" onClick={loginGoogle} />
-                        <AuthButton icon={faGithub} label="Sign In With GitHub" onClick={loginGithub} />
-                        <AuthButton icon={faGitlab} label="Sign In With GitLab" onClick={loginGitlab} />
-                        <AuthButton icon={faDiscord} label="Sign In With Discord" onClick={loginDiscord} />
-                    </div>
-                ) : (
-                    <SignOutButton onClick={() => logout()} />
-                )}
-            </div>
-        </>
+        <div className='flex flex-wrap gap-4 w-full '>
+            {!session ?
+                <div className="flex flex-col md:flex-row justify-center gap-2 p-4 w-full">
+                    <AuthButton icon={faGoogle} label="Sign In With Google" onClick={loginGoogle} />
+                    <AuthButton icon={faGithub} label="Sign In With GitHub" onClick={loginGithub} />
+                    <AuthButton icon={faGitlab} label="Sign In With GitLab" onClick={loginGitlab} />
+                    <AuthButton icon={faDiscord} label="Sign In With Discord" onClick={loginDiscord} />
+                </div>
+                :
+                <SignOutButton onClick={() => logout()} />
+            }
+        </div>
     );
 };
 
