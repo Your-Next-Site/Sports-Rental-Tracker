7c8e2f094769ac8596ed8a049b7b8eddcb9ffb07
Patrick MacDonald <patrick@patmac.ca>
2025-06-01 18:41:46 -0600

    minor css change

--- components/ui/buttons/pagination-button.tsx
+++ components/ui/buttons/pagination-button.tsx
@@ -3,7 +3,7 @@ import { PaginationButtonProps } from "@/types/types";
 export function PaginationButton({ onClick, disabled, children }: PaginationButtonProps) {
     return (
         <button
-            className="rounded-md bg-buttoncolormain hover:brightness-90 hover:cursor-pointer select-none w-12 h-12 text-center p-1.5 text-white"
+            className="rounded-md bg-buttoncolormain hover:brightness-90 hover:cursor-pointer select-none w-12 h-12 text-center p-1.5 hover:bg-buttoncolorsecend text-white"
             onClick={onClick}
             disabled={disabled}
         >
