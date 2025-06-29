28c0cb0caa8080c8d23f5daa115ccdee7228dc4f
Patrick MacDonald <patrick@patmac.ca>
2025-06-29 15:12:07 -0600

    added clerk

--- components/ui/banner/banner.tsx
+++ components/ui/banner/banner.tsx
@@ -1,11 +1,24 @@
-
+import { OrganizationSwitcher, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Banner() {
     return (
         <div className="shadow-2xl bg-white/20 backdrop-filter backdrop-blur-lg rounded-md p-3 md:p-8">
-            <h1 className="text-2xl sm:text-4xl text-bannertext font-bold text-center">
-                Sports Rental Tracker
-            </h1>
+            <header className="flex justify-between items-center p-4 gap-4 h-16">
+                <div className="flex-1"></div>
+                <h1 className="text-2xl sm:text-4xl text-bannertext font-bold text-center flex-1">
+                    Sports Rental Tracker
+                </h1>
+                <div className="flex gap-4 justify-end flex-1">
+                    <SignedOut>
+                        <SignInButton />
+                        <SignUpButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                        <OrganizationSwitcher />
+                    </SignedIn>
+                </div>
+            </header>
         </div>
     );
 }
