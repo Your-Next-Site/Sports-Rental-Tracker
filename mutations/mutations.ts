53d164c01c8954bd777c068663606be7d3f26047
Patrick MacDonald <patrick@patmac.ca>
2025-06-29 23:50:24 -0600

    fixed bad code

--- mutations/mutations.ts
+++ mutations/mutations.ts
@@ -56,70 +56,13 @@ export const useEndRental = (currentPage: number, setPage: (page: number) => voi
 
     return useMutation({
         mutationFn: (raftOnWaterId: number) => endRental(raftOnWaterId),
-        onSuccess: (_, raftOnWaterId) => {
-            // Remove trip locally before refetch
-            queryClient.setQueryData(["trips", true, currentPage], (oldData: { trips: Trip[] }) => {
-                if (!oldData) return oldData;
-                const updatedTrips = oldData.trips.filter((trip) => trip.id !== raftOnWaterId);
-
-                // If no trips remain, move to previous page
-                if (updatedTrips.length === 0 && currentPage > 0) {
-                    setPage(currentPage - 1);
-                }
-
-                return { ...oldData, trips: updatedTrips };
-            });
-            queryClient.clear();
-        },
-        onError: (error) => {
-            console.error('Mutation error:', error);
-        }
-    });
-};
-
-export const useToggleAdmin = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: (email: string) => {
-            return toggleAdmin(email);
-        },
         onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['users'] });
+            queryClient.invalidateQueries({ queryKey: ["trips", true, currentPage] });
         },
         onError: (error) => {
             console.error('Mutation error:', error);
         }
     });
 };
-
-export const useToggleEmployee = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: (email: string) => {
-            return toggleEmployee(email);
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['users'] });
-        },
-        onError: (error) => {
-            console.error('Mutation error:', error);
-        }
-    });
-};
-
-// export const useSearchTrips = () => {
-//     const queryClient = useQueryClient();
-//     return useMutation({
-//         mutationFn: ({ guestName, departureDate }: { guestName: string, departureDate: Date }) => {
-//             return searchTrips(guestName, departureDate);
-//         },
-//         onSuccess: (data: Trip[]) => {
-//             queryClient.invalidateQueries({ queryKey: ['searchPageTrips'] });
-//         },
-//         onError: (error) => {
-//             console.error('Mutation error:', error);
-//         }
-//     });
-// };
 
 
