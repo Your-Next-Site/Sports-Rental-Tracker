4bfe162a0b2b7fb481a7ac41eaebc64201ea9014
Patrick MacDonald <patrick@patmac.ca>
2025-07-14 12:59:35 -0600

    code cleanup

--- components/ui/forms/departure-form.tsx
+++ components/ui/forms/departure-form.tsx
@@ -4,28 +4,24 @@ import MainContainer from "../containers/main-container";
 import Inputs from "./inputs";
 import { ItemTypes } from "@/types/types";
 import { Suspense } from "react";
+import InputDepartureFormFallback from "../fallbacks/input-departure-form-fallback";
 
 export default function DepartureForm({ itemTypesPromise }: { itemTypesPromise: Promise<ItemTypes[]> }) {
     const { mutate, isPending, isError } = useAddRentalStart();
 
-    return (<>
-        <h1 className="text-2xl">Show a guests departure </h1>
-        <form
-            action={mutate}
-            className="flex flex-col gap-2 "
-        >
-            <Suspense fallback={
-                <div className="flex flex-col md:flex-row gap-4">
-                    <div className="flex flex-col md:flex-row w-full p-2 gap-4 justify-center items-center">
-                        <h1>Loading...</h1>
-                    </div>
-                </div>}
+    return (
+        <>
+            <h1 className="text-2xl">Show a guests departure </h1>
+            <form
+                action={mutate}
+                className="flex flex-col gap-2 "
             >
-                <Inputs itemTypesPromise={itemTypesPromise} isPending={isPending} />
-            </Suspense>
-            {isError && <p className="text-red-500">Error adding trip</p>}
-        </form>
-    </>
+                <Suspense fallback={<InputDepartureFormFallback />}>
+                    <Inputs itemTypesPromise={itemTypesPromise} isPending={isPending} />
+                </Suspense>
+                {isError && <p className="text-red-500">Error adding trip</p>}
+            </form>
+        </>
     );
 }
 
