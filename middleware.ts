774990d1dd151e8f842fbba497705d5a8fe64970
Patrick MacDonald <patrick@patmac.ca>
2025-07-15 18:04:27 -0600

    clean up middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
-const isOrgRoute = createRouteMatcher(['/(.*)'])
 const isProtectedRoute = createRouteMatcher(['/main-rental-page(.*)'])
 const isAdminRoute = createRouteMatcher(['/admin(.*)'])
-
 
 export default clerkMiddleware(async (auth, req) => {
   // const clerk = await clerkClient()
@@ -21,25 +19,6 @@ export default clerkMiddleware(async (auth, req) => {
     url.pathname = '/'
     return NextResponse.redirect(url)
   }
-  // // console.log("ORG ID: ", orgId)
-  // if (isOrgRoute(req)) {
-  //   if (orgId) {
-  //     // Check which plan the org has and set appropriate limit
-  //     if (has({ plan: 'basic_10_people_org' })) {        // Set limit for basic plan (e.g., 10 users)
-  //       // console.log("HasPlan 10 : ", (has({ plan: 'basic_10_people_org' })))
-  //       clerk.organizations.updateOrganization(orgId, {
-  //         maxAllowedMemberships: 10
-  //       })
-
-  //     }
-  //     else if (has({ plan: 'pro_25_people_org' })) {
-  //       // console.log("HasPlan 25 : ", (has({ plan: 'pro_25_people_org' })))
-  //       clerk.organizations.updateOrganization(orgId, {
-  //         maxAllowedMemberships: 25
-  //       })
-  //     }
-  //   }
-  // }
 })
 
 export const config = {
